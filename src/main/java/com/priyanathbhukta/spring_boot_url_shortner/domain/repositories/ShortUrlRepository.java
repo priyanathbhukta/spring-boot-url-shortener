2257d1c7a05e14957d5acc98908cf7a26636fb60
Priyanath Bhukta <priyanathbhukta@gmail.com>
2025-08-12 23:57:59 +0530

    database-integration implementation

---
+++ src/main/java/com/priyanathbhukta/spring_boot_url_shortner/domain/repositories/ShortUrlRepository.java
@@ -0,0 +1,13 @@
+package com.priyanathbhukta.spring_boot_url_shortner.domain.repositories;
+
+import com.priyanathbhukta.spring_boot_url_shortner.domain.entities.ShortUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ShortUrlRepository extends JpaRepository<ShortUrl,Long> {
+
+    @Query("select su from ShortUrl su where su.isPrivate=false order by su.createdAt desc ")
+    List<ShortUrl> findPublicShortUrls();
+}
